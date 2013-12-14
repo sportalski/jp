59b06c716517c66ec9742a824c011d45b5fdc662
sportalski <shace209@o2.pl>
2013-12-14 19:40:06 +0100

    Create petle-1.c

---
+++ petle-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x,i;
+    x=1;
+    i=1;
+    x = pow(2, 0);
+    while (x <= 2010) {
+
+
+        printf("2^%d = %d\n", i, x);
+        x = pow(2, i);
+        ++i;
+    }
+    return 0;
+}
